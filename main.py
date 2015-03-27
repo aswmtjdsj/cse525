52086d21e16cfd583f5073bbfd39c2573f1c8c2c
Xiang Yu <bupt.aswmtjdsj@gmail.com>
2015-03-27 18:56:50 -0400

    main updated and the whole nxt package

--- main.py
+++ main.py
@@ -38,7 +38,33 @@ if __name__ == '__main__':
     # color sensor v2
     color_v2 = Colorv2(brick, PORT_1)
     color_v2.debug = False
+    move_power = 100
+    turn_power = 70
+    move_time = 0.2
+    turn_time = 0.1
 
+    def move(power, run_time, direction):
+        scale = (1 if direction == 1 else -1)
+        print "move {0}".format("forward" if direction == 0 else "backward")
+        motor[0].run(scale * power)
+        motor[1].run(scale * power)
+        time.sleep(run_time)
+        motor[0].brake()
+        motor[1].brake()
+
+    def turn(direction, scale):
+        if direction == 'NE':
+            pass
+        elif direction == 'SE':
+            pass
+        elif direction == 'SW':
+            pass
+        elif direction == 'NW':
+            pass
+        else:
+            print 'wrong direction'
+
+    count = 0
     while 1:
         # sense
         # light.set_illuminated(False)
@@ -53,17 +79,14 @@ if __name__ == '__main__':
         elif rgb_is_white(color_v) == True:
             print 'white'
         elif rgb_is_red(color_v) == True:
-            print 'red'
+            print 'red and exit'
+            break
         else:
             print 'other'
 
-        # # run
-        # print "run"
-        # motor[0].run(100)
-        # motor[1].run(100)
-        # time.sleep(0.5)
-        # motor[0].brake()
-        # motor[1].brake()
+        # run
+        move(move_power, move_time, count % 2)
+        count += 1
 
         # # turn
         # m_id = random.randint(0, 1)
