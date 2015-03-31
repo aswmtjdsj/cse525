04746d8263cf3458fc71995b78583741251e7d5a
尹心轶 <zephyryin@inshins-MacBook-Air.local>
2015-03-31 02:15:44 -0400

    test color sensor option

--- followBlack.py
+++ followBlack.py
@@ -3,11 +3,12 @@ from baseClass import *
 
 rangeRover = Car()
 while 1:
-
     print '     start with 1 sensor :   1'
     print '     start with 2 sensor :   2'
     print '     draw map            :   3'
-    print '     quit                :   4'
+    print '     test color sensor   :   4'
+    print '     quit                :   5'
+
     print 'please input cmd:'
     cmd = raw_input()
     if cmd == '1':
@@ -19,6 +20,10 @@ while 1:
             rangeRover.readTurnFromLog()
         rangeRover.drawMap()
     elif cmd == '4':
+        for i in range(10):
+            print rangeRover.getCurrentColor(True)
+            time.sleep(2)
+    elif cmd == '5':
         break
     else:
         print 'error command'
