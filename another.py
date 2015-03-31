af7eb298bea3b35bff096ad7e67870d2e4a19a54
Xiang Yu <bupt.aswmtjdsj@gmail.com>
2015-03-31 02:28:14 -0400

    draw not done

--- another.py
+++ another.py
@@ -26,30 +26,24 @@ def get_sensor_rgb(color_sensor):
 
 if __name__ == '__main__':
 
+    log_f = "track.log"
+
     # get lego
     brick = nxt.find_one_brick()
 
     # get motors
     motor = [nxt.Motor(brick, nxt.PORT_A), nxt.Motor(brick, nxt.PORT_B)]
 
-    # light = Light(brick, PORT_1)
-    # color = Color20(brick, PORT_1)
-    # color sensor v2
+    # get sensors
     sensors = [Colorv2(brick, PORT_1), Colorv2(brick, PORT_4)]
     sensors[0].debug = False
     sensors[1].debug = False
-    move_power = 80
-    detect_power = 80
-    turn_power = 100
-    move_time = 0.2
-    turn_time = 0.1
-    detect_time = 0.01
 
-    def stop():
-        motor[0].brake()
+    def stop(): # brake the motors
+        motor[0].brake() # not sync, errors may accumulate
         motor[1].brake()
 
-    def self_turn(direction, power, period):
+    def self_turn(direction, power, period): # sensor in front of left wheel, self-turn means only turning right wheel
         motor[1].run(direction * power)
 
     def try_and_turn(direction, power, period, limit):
@@ -60,6 +54,11 @@ if __name__ == '__main__':
         
         first_dir = -1 if direction == False else 1
         second_dir = 1 if direction == False else -1
+
+        # <----
+        # ---->
+        #      ---->
+        #      <----
         for i in range(limit): # one dire
             if get_sensor_rgb(sensors[0]) == 'black' or get_sensor_rgb(sensors[0]) == 'red':
                 stop()
@@ -72,7 +71,8 @@ if __name__ == '__main__':
                 stop()
                 return (i+1) * second_dir
             self_turn(second_dir, power, period)
-        stop()
+        stop() # add this stop(brake) to make the inertia the same
+
         for i in range(limit): # back and the other dir
             if get_sensor_rgb(sensors[0]) == 'black' or get_sensor_rgb(sensors[0]) == 'red':
                 stop()
@@ -87,7 +87,7 @@ if __name__ == '__main__':
             self_turn(first_dir, power, period)
         stop()
 
-        return None
+        return None # no detection of black
 
     def move(power, run_time, direction):
         scale = (1 if direction == 1 else -1)
@@ -95,49 +95,94 @@ if __name__ == '__main__':
         motor[0].run(scale * power)
         motor[1].run(scale * power)
 
-    count = 0
-    running = True
-    try_number = 0
+    cmd = raw_input("""Command ("1" for line-following, "2" for re-drawing map)>""")
+    if cmd == "2":
+        f = open(log_f, "r")
+        for line in f:
+            biubiu = line.strip('\n').split(' ')
+            if biubiu[0] == 'move':
+                move(int(biubiu[1]), float(biubiu[2]), int(biubiu[3]))
+                time.sleep(float(biubiu[2]))
+                stop()
+                print 'move', int(biubiu[1]), float(biubiu[2]), int(biubiu[3])
+            elif biubiu[0] == 'turn':
+                stop()
+                kaka = float(biubiu[2])
+                times = kaka / 0.18
+                times = int(times)
+                for i in range(times):
+                    self_turn(-1 if kaka < 0 else 1, int(biubiu[1]), abs(kaka))
+                    time.sleep(float(biubiu[1]))
+                    stop()
+                print 'turn', -1 if kaka < 0 else 1, int(biubiu[1]), abs(kaka)
+            else:
+                stop()
+        f.close()
+    else:
 
-    update_limit = 5
-    update_step = 5
-    turn = None
-    turned = False
-    while running:
-        color = get_sensor_rgb(sensors[0])
-        print 'update_limit: ', update_limit
-        print 0, color
-        if color == 'red':
-            stop()
-            print 'red and exit'
-            break
-        elif color == 'black':
-            try_number = 0
-            print 'black and go'
-            if turned == True:
-                move(move_power, 0.01, 1)
+        f = open(log_f, "w")
+        # set conf
+        move_power = 80
+        turn_power = 70 
+        detect_power = 80
+        move_time = 0.2
+        detect_time = 0.01
+
+        count = 0
+        running = False
+
+        update_limit = 5
+        update_step = 5 # seem like iterative deepening search
+        turn = None # try_and_turn return value
+        turned = False # last step, turned or not
+
+        while True:
+            color = get_sensor_rgb(sensors[0])
+            print 'update_limit: ', update_limit
+            print 0, color
+            if color == 'red': # time to stop? or the first time?
+                if running == False:
+                    move(move_power, 0.1, 1) # first trigger
+                    print >> f, "move", move_power, 0.1, 1
+                    running = True
+                else:
+                    stop()
+                    print >> f, "exit"
+                    print 'red and exit'
+                    break
+
+            elif color == 'black':
+                print 'black and go'
+                if turned == True: # after try_an_turn, move forward carefully
+                    print >> f, "move", move_power, 0.01, 1
+                    move(move_power, 0.01, 1)
+                else:
+                    print >> f, "move", move_power, 0.05, 1
+                    move(move_power, 0.05, 1)
+                turned = False
+
+            elif color == 'white':
+                stop()
+                turn = try_and_turn(True, detect_power, detect_time, update_limit)
+                print 'turn', turn
+                if turn != None:
+                    print >> f, "turn", detect_power, detect_time * turn
+                    update_limit = 5
+                    turned = True
+                    turn = None
+                else:
+                    update_limit += update_step
+                print 'white and turn'
             else:
-                move(move_power, 0.05, 1)
-            turned = False
-        elif color == 'white':
-            stop()
-            turn = try_and_turn(True, detect_power, detect_time, update_limit)
-            print 'turn', turn
-            if turn != None:
-                update_limit = 5
-                turned = True
-                turn = None
-            else:
-                update_limit += update_step
-            print 'white and turn'
-        else:
-            stop()
-            turn = try_and_turn(False, detect_power, detect_time, update_limit)
-            print 'turn', turn
-            if turn != None:
-                update_limit = 5
-                turned = True
-                turn = None
-            else:
-                update_limit += update_step
-            print 'other and c-turn'
+                stop()
+                turn = try_and_turn(False, detect_power, detect_time, update_limit)
+                print 'turn', turn
+                if turn != None:
+                    print >> f, "turn", detect_power, detect_time * turn
+                    update_limit = 5
+                    turned = True
+                    turn = None
+                else:
+                    update_limit += update_step
+                print 'other and c-turn'
+        f.close()
